172583f9482f963e0cdbae648cef22de36358434
Rateland <134687101+Rateland@users.noreply.github.com>
2024-05-03 15:08:38 +0200

    Stylage
    Plus de stylage, moins de laideur

--- static/js/style.js
+++ static/js/style.js
@@ -2,11 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
     // Animation de survol du panier
     const panierLink = document.querySelector('.panier-link a');
     if (panierLink) {
+        const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary-color').trim();
+
         panierLink.addEventListener('mouseenter', function () {
-            panierLink.style.color = 'red';  // Change la couleur lors du survol
+            panierLink.style.color = primaryColor; // Utiliser la couleur définie
         });
         panierLink.addEventListener('mouseleave', function () {
             panierLink.style.color = '';  // Restaure la couleur d'origine
         });
     }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            alert('Billet ajouté au panier !');
+        });
+    });
+});
+
+/* Pour le billet */
+document.addEventListener('DOMContentLoaded', function () {
+    const billetAction = document.querySelector('.billet-action');
+    billetAction.addEventListener('click', function () {
+        alert('Billet ajouté au panier !');
+    });
 });
