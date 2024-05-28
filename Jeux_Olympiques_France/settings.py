25d7559b03b406b2cd1ac1e70afccf9fc9a63a75
Rateland <134687101+Rateland@users.noreply.github.com>
2024-05-28 17:26:06 +0200

    Extension de 'base'

--- Jeux_Olympiques_France/settings.py
+++ Jeux_Olympiques_France/settings.py
@@ -53,7 +53,7 @@ LOGGING = {
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
