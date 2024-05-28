ec263a2b9e7bb96be0e81e7362d5d9e39657cd71
Rateland <134687101+Rateland@users.noreply.github.com>
2024-05-28 22:16:33 +0200

    Repasser en HTML

--- accounts/views.py
+++ accounts/views.py
@@ -23,11 +23,11 @@ def signup(request):
         # Validation des mots de passe
         if password != password_confirm:
             messages.error(request, 'Les mots de passe ne correspondent pas.')
-            return render(request, 'accounts/signup.jinja2')
+            return render(request, 'accounts/signup.html')
 
         if User.objects.filter(username=username).exists():
             messages.error(request, 'Ce nom d’utilisateur est déjà pris.')
-            return render(request, 'accounts/signup.jinja2')
+            return render(request, 'accounts/signup.html')
 
         user = User.objects.create_user(
             username=username,
@@ -39,7 +39,7 @@ def signup(request):
         login(request, user)
         return redirect('accueil')
 
-    return render(request, 'accounts/signup.jinja2')
+    return render(request, 'accounts/signup.html')
 
 def login_user(request):
     if request.method == "POST":
@@ -58,9 +58,9 @@ def login_user(request):
                 return redirect('accueil')
 
         messages.error(request, 'Courriel ou mot de passe incorrect.')
-        return render(request, 'accounts/login.jinja2')
+        return render(request, 'accounts/login.html')
     
-    return render(request, 'accounts/login.jinja2')
+    return render(request, 'accounts/login.html')
 
 @login_required
 def client_page(request):
@@ -89,7 +89,7 @@ def client_page(request):
         email_form = EmailForm(instance=request.user)
         password_form = CustomPasswordChangeForm(user=request.user)
 
-    return render(request, 'accounts/client_page.jinja2', {
+    return render(request, 'accounts/client_page.html', {
         'name_form': name_form,
         'email_form': email_form,
         'password_form': password_form
